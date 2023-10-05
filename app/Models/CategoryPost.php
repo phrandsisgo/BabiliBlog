dc492a9e4ee531f238c22f5e6c295a8e9b25ed20
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-10-05 11:04:01 +0200

    imports added for Models

--- app/Models/CategoryPost.php
+++ app/Models/CategoryPost.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryPost extends Model
 {
+    //this is the connector for many to many relationship so it doesn't need functions for the connections
     use HasFactory;
 }

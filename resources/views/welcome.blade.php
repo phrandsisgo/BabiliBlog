12704d03665afb21019bcf181c8fd800aaa6ad6e
Scarrus666 <scarrus@gmail.com>
2023-10-09 16:49:02 +0200

    commit for routes extensive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,6 @@
 @endsection
 
 @section('content')
-<div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla dictum libero, eu convallis sapien. Fusce ut sollicitudin velit. Donec ac sagittis elit.</p>
-</div>
 @endsection
 
 @section('footer')

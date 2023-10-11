6090a5ec4f8d0d1d8d055cf65de37d65e40286a7
ABoleiro11 <aboleiro@gmail.com>
2023-10-11 11:13:28 +0200

    push for all

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,30 +17,37 @@
 @endsection
 
 @section('content')
-    <header>
+ 
 
+<div class="main">
+    <div class="header">
+        <div>
+            <h1>Header</h1>
+        </div>
+    </div>             
     
-        <h1>Techwizards</h1>
+    <div class="content">
 
-        
-    </header>
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Post</a></li>
-            <li><a href="#">Thema 1</a></li>
-            <li><a href="#">Thema 2</a></li>
-            <li><a href="#">Thema 3</a></li>
-        </ul>
-    </nav>
+        <div class="content-1">
+            <p>
+                Hier kommt der Haupttext
+            </p>
+        </div>
 
-   
-        
-    
-    <div class="content-container">
-        <!-- Hier können Sie Ihren Inhalt platzieren -->
-        
+        <div class="content-2">
+            <p>
+                Hier kommen die anderen
+            </p>
+        </div>
+
+        <div class="footer">
+            <p>&copy; 2023 TechWizzards</p>
+        </div>
     </div>
+</div>
+        
+    
+
 
     @endsection
 

dc9c4ae5678f6e12c723a59e7d63652e30b87ef3
ABoleiro11 <aboleiro@gmail.com>
2023-10-16 09:49:08 +0200

    Alex G Problems

--- resources/views/displayPosts.blade.php
+++ resources/views/displayPosts.blade.php
@@ -17,3 +17,37 @@
     @endforeach
 </body>
 </html>
+
+{{-- WRITTEN BY SCARRUS --}}
+
+@extends('/components/layout')
+
+
+@section('head')
+@endsection
+
+@section('header')
+@endsection
+
+@section('content')
+
+    <div class="content">
+
+    {{-- @foreach ($posts as $post)  --}}
+    @foreach ($posts as $post) 
+    <div class="content-1"> 
+        <p class="date">{{ date('d.m.y', strtotime($post->created_at)) }}</p>
+        <h3>{{ $post->title }}</h3>
+        <p>{{ $post->content }}</p>
+        <p>the id is {{$post -> id}}</p>
+        <a href="/show/{{$post->id}}">View Show</a>
+    </div>
+    @endforeach
+    </div>
+
+
+    {{-- @endforeach --}}
+@endsection
+
+@section('footer')
+@endsection

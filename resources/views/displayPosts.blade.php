c38e0c5a56152d0ee4626031d01b890311008947
ABoleiro11 <aboleiro@gmail.com>
2023-10-16 10:57:49 +0200

    merging things

--- resources/views/displayPosts.blade.php
+++ resources/views/displayPosts.blade.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Display all the posts</title>
-</head>
-<body>
-@foreach ($posts as $post) 
-    <div class="tweet-container"> 
-        <p class="date">{{ date('d.m.y', strtotime($post->created_at)) }}</p>
-        <h3>{{ $post->title }}</h3>
-        <p>{{ $post->content }}</p>
-        <p>the id is {{$post -> id}}</p>
-        <a href="/show/{{$post->id}}">View Show</a>
-    </div>
-    @endforeach
-</body>
-</html>
-
-{{-- WRITTEN BY SCARRUS --}}
-
 @extends('/components/layout')
 
 
@@ -40,7 +18,19 @@
         <h3>{{ $post->title }}</h3>
         <p>{{ $post->content }}</p>
         <p>the id is {{$post -> id}}</p>
-        <a href="/show/{{$post->id}}">View Show</a>
+
+<!-- Folgendes muss ein Button -->
+        <button class="view-button">
+            <a href="/show/{{$post->id}}">View Show</a>
+        </button>
+
+        
+            <h4><a href="/show/{{$post->id}}">View Show</a></h4>
+        
+
+
+
+
     </div>
     @endforeach
     </div>

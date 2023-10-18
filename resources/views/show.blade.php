15f3dcd05489862f2ced3b360dde3ce9d3ea8d1f
Scarrus666 <scarrus@gmail.com>
2023-10-18 17:03:00 +0200

    show blade comments fix

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -34,10 +34,12 @@
             
             @endauth
             <h2>Comments</h2>
+            <br>
             <ul>
 
                 @foreach($post->comments as $comment)
                     <li>{{ $comment->content }}</li>
+                    <br><br>
                     @if(auth()->check())
                         @if(auth()->user()->id == $comment->user_id)
                         <a href="/kommentar_bearbeiten/{{$comment -> id}}"> Bearbeiten</a>
@@ -51,34 +53,38 @@
                 @endforeach
             </ul>
         </div>
-    </div>
-    <div class="content">
-    <div class="content-1">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Create a Comment</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="/new_comment/{{$post ->id}}">
-                            @csrf
+    @auth()
 
-                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                            <input type="hidden" name="post_id" value="{{$post ->id}}">
+        <div class="content-1">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header"><h3>Create a Comment</h3></div>
 
-                            <div class="form-group">
-                                <label for="content">Comment</label>
-                                <textarea id="content" name="content" class="form-control" required></textarea>
-                            </div>
+                        <br>
+                        <div class="card-body">
+                            <form method="POST" action="/new_comment/{{$post ->id}}">
+                                @csrf
 
-                            <button type="submit" class="btn btn-primary">Create a Comment</button>
-                        </form>
+                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                                <input type="hidden" name="post_id" value="{{$post ->id}}">
+
+                                <div class="form-group">
+                                    <label for="content"></label>
+                                    <textarea id="content" name="content" class="form-control" required></textarea>
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary"><h4>Post Comment</h4></button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    @endauth
     </div>
-</div>
+
 @endsection
 
 @section('footer')

ce2e694c2f81a2a6c939e9c1d1f9d06aca5e8164
Scarrus666 <scarrus@gmail.com>
2023-10-18 12:32:53 +0200

    design

--- resources/views/post_bearbeiten.blade.php
+++ resources/views/post_bearbeiten.blade.php
@@ -13,6 +13,7 @@
 
 <div class="content">
     <div class="content-1">
+        <div class="content-in">
         <form action="/post_update/{{$post->id}}" method="POST">
             @csrf
             <br>
@@ -20,12 +21,13 @@
             <br><br>
             <input type="text" value="{{$post->title}}" name="title" class="form-control formStyle @error('title') is-invalid @enderror title-input">
             <br><br><br>
-            <label for="content"><h3>Inhalt</h3></label>
+            <label for="content"><h2>Inhalt</h2></label>
             <br><br>
             <textarea name="content" class="form-control formStyle @error('content') is-invalid @enderror content-input" rows="4">{{ $post->content }}</textarea>
             <br><br>
-            <button type="submit"><h3>Speichern</h3></button>
+            <button type="submit"><h4>Speichern</h4></button>
         </form>
+        </div>
     </div>
 </div>
 

e7ad2b7d5b5207b32e09002fee63658edc904294
Scarrus666 <scarrus@gmail.com>
2023-10-19 14:39:34 +0200

    me

--- resources/views/displayPosts.blade.php
+++ resources/views/displayPosts.blade.php
@@ -20,11 +20,18 @@
 
     @foreach ($posts->sortByDesc('created_at') as $post) 
     <div class="content-1"> 
-        <p class="date">{{ date('d.m.y', strtotime($post->created_at)) }}</p>
+        <h4>{{ $post->user->name }}</h4>
+        <br>
+        {{-- <p class="date">{{ date('d.m.y', strtotime($post->created_at)) }}</p> --}}
         <h3>{{ $post->title }}</h3>
         <p>{{ $post->content }}</p>
+        <h4>{{ date('d.m.y', strtotime($post->created_at)) }}</h4>
+
+{{--    DEV ONLY     
         <p>the id is {{$post -> id}}</p>
-        <p>the user ID is: {{$post -> user_id}}</p>
+        <p>the user ID is: {{$post -> user_id}}</p> 
+--}}
+
         <a href="/show/{{$post->id}}"><br><button class="submit-btn">View</button></a>
     </div>
     @endforeach

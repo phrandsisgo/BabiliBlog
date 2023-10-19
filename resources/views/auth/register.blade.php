983ab1e938989a21c30c6f465629c590b15dba37
ABoleiro11 <aboleiro@gmail.com>
2023-10-19 11:07:14 +0200

    again

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,10 @@
     <div class="content">
 
         <div class="content-1">
+
+            <h1>Registrieren</h1>
+            <br><br>
+
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
             
@@ -71,85 +75,3 @@
 
 @section('footer')
 @endsection
-
-
-<!-- 
-     @extends('/components/layout')
-
-
-@section('head')
-@endsection
-
-@section('header')
-@endsection
-
-@section('content')
-
-    <div class="content">
-
-    {{-- @foreach ($posts as $post)  --}}
-        <div class="content-1">
-
-            <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <br>
-            <x-input-label for="email" :value="__('Email')" /><br>
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /><br>
-            <x-input-error :messages="$errors->get('email')" class="mt-2" /><br>
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <br>
-            <x-input-label for="password" :value="__('Password')" />
-
-            <br>
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-            <br>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            <br>
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-        
-        
-        <br><br><br>
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-        </div>
-    </form>
-
-        </div>
-        
-    </div>
-
-
-    {{-- @endforeach --}}
-@endsection
-
-@section('footer')
-@endsection //
-<! -->

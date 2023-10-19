a5ee8dd47ae73d0b0d5af8157fb1cf3fdb4088f2
ABoleiro11 <aboleiro@gmail.com>
2023-10-19 09:46:57 +0200

    Impressum

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -1,3 +1,5 @@
+// Written by Alex g
+
 @extends('/components/layout')
 
 
@@ -13,31 +15,34 @@
         <div class="content-1">
 
             <h1>Impressum</h1>
-
-            <p>Verantwortlich fuer den Inhalt <br>
             <br>
-            TechWizzards Alex G., Alex I., Baris, Binyam, Cyrill, Francisco und Luis <br></p>
-            <br>
+            <h3>Verantwortlich fuer den Inhalt:</h3> <br>
+            
+            <h3>TechWizzards Alex G., Alex I., Baris, Binyam, Cyrill, Francisco und Luis</h3><br>
+            
             Opportunity Zuerich, Schweiz
             <br><br>
 
-            Ueber uns: <br>
-            <br>
+            <h3>Ueber uns:</h3> <br>
+            
             Wir sind die letzten Mohikaner des Opportunity Kurses Web Devs. Wir begannen anfangs Mai 2023 mit dem 6 monatigen Kurs. Einige hatten 
-            Vorerfahrungen mit dem coden, andere waren Neulige im Bereich der Web Entwicklung. Fuer diesen Gruppenprojekt haben wir rund 3 Wochen Zeit um 
+            Vorerfahrungen mit dem coden, andere waren Neulige im Bereich der Web Entwicklung. Fuer diesen Gruppenprojekt haben wir rund 3 Wochen Zeit, um 
             unsere Seite zu kreiren. <br>
             <br>
-            Rechtliche Hinweise:
-            - TechWizzards ist eine Blo<span class="ag">g</span>p<span class="ag">l</span>attf<span class="ag">o</span>rm.
-            - Die Inhalte dieser We<span class="ag">b</span>s<span class="ag">i</span>te <span class="ag">w</span>urden m<span class="ag">i</span>t g<span class="ag">r</span>oesster Sorgfalt erstellt. Fuer <span class="ag">d</span>ie Richtigkeit, Voll<span class="ag">s</span>taendigkeit und Aktualitaet der Inhalte uebernehmen wir jed<span class="ag">o</span>ch keine Gewaehr.
-            - Wir sind nicht verantwort<span class="ag">l</span>ich für externe Links zu Websites <span class="ag">D</span>ritter.
-            - Diese Website und ihre Inh<span class="ag">a</span>l<span class="ag">t</span>e sind urheberrechtlich geschuetzt. Jegliche Verwendung, Vervielfaeltigung oder Verbreitung ohne schriftliche Genehmigung ist untersagt.
-
-            Datenschutz:
-            - Datenschutz und Sicherheit sind uns wichtig. 
-
-            Kontakt:
-            - Bei Fragen oder Anliegen koennen Sie uns unter (name des jeweiligen Autors)@example.ch kontaktieren.
+            <h3>Rechtliche Hinweise:</h3> <br>
+            
+            TechWizzards ist eine Blo<span class="ag">g</span>p<span class="ag">l</span>attf<span class="ag">o</span>rm.
+            Die Inhalte dieser We<span class="ag">b</span>s<span class="ag">i</span>te <span class="ag">w</span>urden m<span class="ag">i</span>t g<span class="ag">r</span>oesster Sorgfalt erstellt. Fuer <span class="ag">d</span>ie Richtigkeit, Voll<span class="ag">s</span>taendigkeit und Aktualitaet der Inhalte uebernehmen wir jed<span class="ag">o</span>ch keine Gewaehr.
+            Wir sind nicht verantwort<span class="ag">l</span>ich fuer externe Links zu Websites <span class="ag">D</span>ritter.
+            Diese Website und ihre Inh<span class="ag">a</span>l<span class="ag">t</span>e sind urheberrechtlich geschuetzt. Jegliche Verwendung, Vervielfaeltigung oder Verbreitung ohne schriftliche Genehmigung ist untersagt.<br>
+            <br>
+            <h3>Datenschutz:</h3> <br>
+            
+            Datenschutz und Sicherheit sind uns wichtig. <br>
+            <br>
+            <h3>Kontakt:</h3> <br>
+            
+            Bei Fragen oder Anliegen koennen Sie uns unter (name des jeweiligen Autors)@example.ch kontaktieren.
 
 
             <p1></p1>

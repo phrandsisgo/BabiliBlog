84c139b4a5a39904d4b7c7fa099c26ad5b5ac12f
ABoleiro11 <aboleiro@gmail.com>
2023-10-19 11:23:40 +0200

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
         <div class="content-1">
 
-            <h1>Registrieren</h1>
+            <h1>Register</h1>
             <br><br>
 
                 <form method="POST" action="{{ route('register') }}">
@@ -21,41 +21,43 @@
             
                     <!-- Name -->
                     <div>
-                        <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        <br>
+                        <x-input-label for="name" :value="__('Name')" /><br>
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" /><br>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" /><br>
                     </div>
                     <br>
             
                     <!-- Email Address -->
                     <div class="mt-4">
-                        <x-input-label for="email" :value="__('Email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <br>
+                        <x-input-label for="email" :value="__('Email')" /><br>
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" /><br>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" /><br>
                     </div>
+                    <br>
             
                     <!-- Password -->
                     <div class="mt-4">
-                        <x-input-label for="password" :value="__('Password')" />
+                        <br>
+                        <x-input-label for="password" :value="__('Password')" /><br>
             
-                        <x-text-input id="password" class="block mt-1 w-full"
-                                        type="password"
-                                        name="password"
-                                        required autocomplete="new-password" />
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" /><br>
             
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" /><br>
                     </div>
+                    <br>
             
                     <!-- Confirm Password -->
                     <div class="mt-4">
-                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <br>
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" /><br>
             
-                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                        type="password"
-                                        name="password_confirmation" required autocomplete="new-password" />
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" /><br>
             
                         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     </div>
+                    <br>
             
                     <div class="flex items-center justify-end mt-4">
                         <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
